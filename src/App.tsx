22a73a5ac1e5b0e04efc815b785a346fe03eeb12
EvgenySychev <samara808@yandex.ru>
2022-09-03 19:56:47 +0400

    refactor Button.module.css, color on hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import './App.css';
 import {CounterAndSettings} from "./Components/CounterAndSettings";
 
 
-function App() {
+export const App = () => {
 
     return (
-        <div className= "App">
+        <div>
             <CounterAndSettings/>
         </div>
     );
-}
-
-export default App;
+};
